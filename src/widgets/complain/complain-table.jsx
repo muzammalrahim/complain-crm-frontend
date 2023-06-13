70502bcd8c88a2013d54ff46f36d7a6d463e6d58
muzammalrahim <creative.joomdev@gmail.com>
2023-06-13 15:59:43 +0500

    Scroll fix

--- src/widgets/complain/complain-table.jsx
+++ src/widgets/complain/complain-table.jsx
@@ -50,7 +50,7 @@ const ComplainTable = ({
         <div className="flex flex-col">
           {/* <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="overflow-hidden"> */}
-          <div className="overflow-auto hover:overflow-scroll">
+          <div className="overflow-x-auto ">
             <div>
               <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                 {complains?.length < 0 && !loading && (
